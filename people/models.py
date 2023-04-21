43d81fa2d9de8f51cf79ea705f4329c6d97cd886
alstacon <alstacon@gmail.com>
2023-04-21 12:24:46 +0300

    people models

---
+++ people/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+
+
+class Person(models.Model):
+    class Meta:
+        abstract = True
+
+    full_name = models.CharField(max_length=255)
+
+
+class Student(Person):
+    class Meta:
+        verbose_name = 'Студент'
+        verbose_name_plural = 'Студенты'
+
+    def __str__(self):
+        return self.full_name
+
+
+class Teacher(Person):
+    degree = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name = 'Преподаватель'
+        verbose_name_plural = 'Преподаватели'
+
+    def __str__(self):
+        return self.full_name
+
+
+class StudentGroup(models.Model):
+    title = models.CharField(max_length=255)
+    students = models.ManyToManyField(Student)
+
+    class Meta:
+        verbose_name = 'Группа обучаемых'
+        verbose_name_plural = 'Группы обучаемых'
+
+    def __str__(self):
+        return self.title

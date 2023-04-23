ee95e763646e78ec69e55874015bf2ccfb3365e8
alstacon <alstacon@gmail.com>
2023-04-23 13:18:12 +0300

    readme + ordering

--- lessons/views.py
+++ lessons/views.py
@@ -3,6 +3,7 @@ from rest_framework import viewsets
 from lessons import models
 from lessons import serializers
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 
 from lessons.filters import LessonFilter
 
@@ -10,6 +11,9 @@ from lessons.filters import LessonFilter
 class StudySubjectViewSet(viewsets.ModelViewSet):
     queryset = models.StudySubject.objects.prefetch_related('lesson')
     default_serializer_class = serializers.StudySubjectSerializer
+    filter_backends = (DjangoFilterBackend, filters.OrderingFilter)
+    ordering_fields = ['title']
+    ordering = ['title']
 
     serializers = {
         'retrieve': serializers.StudySubjectDetailSerializer
@@ -23,8 +27,10 @@ class LessonViewSet(viewsets.ModelViewSet):
     queryset = models.Lesson.objects.select_related('study_subject', 'teacher',
                                                     'student_group').prefetch_related('missing_students')
     default_serializer_class = serializers.LessonSerializer
-    filter_backends = (DjangoFilterBackend,)
+    filter_backends = (DjangoFilterBackend, filters.OrderingFilter)
     filterset_class = LessonFilter
+    ordering_fields = ['date']
+    ordering = ['date']
 
     serializers = {
         'create': serializers.LessonCreateSerializer,
--- people/views.py
+++ people/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.filters import OrderingFilter
 
 from people import models
 from people import serializers
@@ -7,16 +8,21 @@ from people import serializers
 class StudentViewSet(viewsets.ModelViewSet):
     queryset = models.Student.objects.all()
     serializer_class = serializers.StudentSerializer
+    ordering = ['full_name']
 
 
 class TeacherViewSet(viewsets.ModelViewSet):
     queryset = models.Teacher.objects.all()
     serializer_class = serializers.TeacherSerializer
+    filter_backends = (OrderingFilter,)
+    ordering_fields = ['full_name']
 
 
 class StudentGroupViewSet(viewsets.ModelViewSet):
     queryset = models.StudentGroup.objects.prefetch_related('students')
     default_serializer_class = serializers.StudentGroupSerializer
+    filter_backends = (OrderingFilter,)
+    ordering_fields = ['id']
 
     serializers = {
         'retrieve': serializers.StudentGroupDetailSerializer,

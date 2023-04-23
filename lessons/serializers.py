824b2914925f3eb2b1e824f9d5fd394477544673
alstacon <alstacon@gmail.com>
2023-04-23 14:10:05 +0300

    date-status logic

--- lessons/serializers.py
+++ lessons/serializers.py
@@ -1,4 +1,5 @@
 from django.db.models import Q
+from django.utils import timezone
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 
@@ -8,7 +9,7 @@ from people import serializers as people_serializers
 
 
 class LessonSerializer(serializers.ModelSerializer):
-    date = serializers.DateTimeField()
+    date = serializers.DateTimeField(format='%d/%m/%Y %H:%M:%S')
     teacher = serializers.SlugRelatedField(
         read_only=True,
         slug_field='full_name'
@@ -29,7 +30,7 @@ class LessonSerializer(serializers.ModelSerializer):
 
 
 class LessonListSerializer(serializers.ModelSerializer):
-    date = serializers.DateTimeField()
+    date = serializers.DateTimeField(format='%d/%m/%Y %H:%M:%S')
     teacher = serializers.SlugRelatedField(
         read_only=True,
         slug_field='full_name'
@@ -48,14 +49,14 @@ class LessonListSerializer(serializers.ModelSerializer):
 
 class LessonCreateSerializer(serializers.ModelSerializer):
     date = serializers.DateTimeField(
-        input_formats=['%Y-%m-%dT%H:%M:%SZ', '%Y-%m-%dT%H:%M:%S', '%Y-%m-%d', '%d/%m/%Y %H:%M:%S'])
+        input_formats=['%d/%m/%Y %H:%M:%S'])
 
     class Meta:
         model = models.Lesson
         fields = '__all__'
 
     def validate(self, attrs: dict) -> dict:
-        """Проверяет входные данные об отсутствующих студентах"""
+        """Проверяет входные данные об отсутствующих студентах и статусе занятия"""
         if 'missing_students' in attrs:
             if not attrs.get('status'):
                 raise ValidationError({
@@ -71,13 +72,18 @@ class LessonCreateSerializer(serializers.ModelSerializer):
                 for student in students:
                     if people_models.StudentGroup.objects.filter(Q(id=attrs['student_group'].id) & Q(students=student)):
                         attrs['missing_students'].append(student)
+        if 'status' in attrs:
+            if attrs['status'] and attrs['date'] > timezone.now():
+                raise ValidationError({
+                    'status': 'Занятия нельзя завершить раньше назначенной даты.'
+                })
 
         return attrs
 
 
 class LessonUpdateSerializer(serializers.ModelSerializer):
     date = serializers.DateTimeField(
-        input_formats=['%Y-%m-%dT%H:%M:%SZ', '%Y-%m-%dT%H:%M:%S', '%Y-%m-%d', '%d/%m/%Y %H:%M:%S'])
+        input_formats=['%d/%m/%Y %H:%M:%S'])
 
     class Meta:
         model = models.Lesson
@@ -103,6 +109,11 @@ class LessonUpdateSerializer(serializers.ModelSerializer):
                     if people_models.StudentGroup.objects.filter(
                             Q(id=student_group.id) & Q(students=student)):
                         validated_data['missing_students'].append(student)
+        if 'status' in validated_data:
+            if validated_data['status'] and validated_data['date'] > timezone.now():
+                raise ValidationError({
+                    'status': 'Занятия нельзя завершить раньше назначенной даты.'
+                })
 
         super().update(instance, validated_data)
         return instance
--- school/tests/lessons/test_lesson.py
+++ school/tests/lessons/test_lesson.py
@@ -100,6 +100,25 @@ class TestLesson(TestCase):
                          ['Введите группу обучаемых, чтобы добавить или изменить отсутствующих.']
                          )
 
+    def test_create_with_incorrect_status(self):
+        self.assertEqual(2, Lesson.objects.all().count())
+        url = reverse('lesson-list')
+        data = json.dumps(
+            {
+                'date': '01/05/2023 10:00:00',
+                'teacher': 1,
+                'study_subject': 1,
+                'status': True,
+            })
+
+        response = self.client.post(url, data=data, content_type='application/json')
+
+        self.assertEqual(status.HTTP_400_BAD_REQUEST, response.status_code)
+        self.assertEqual(2, Lesson.objects.all().count())
+        self.assertEqual(response.json()['status'],
+                         ['Занятия нельзя завершить раньше назначенной даты.']
+                         )
+
     def test_get_list(self):
         url = reverse('lesson-list')
         response = self.client.get(url)
@@ -121,7 +140,7 @@ class TestLesson(TestCase):
     def test_update(self):
         url = reverse('lesson-detail', args=[self.lesson_1.id])
         data = {
-            'date': '01/05/2023 10:00:00',
+            'date': '01/02/2023 10:00:00',
             'teacher': 1,
             'study_subject': 2,
             'student_group': 1,
@@ -171,7 +190,7 @@ class TestLesson(TestCase):
     def test_update_invalid_missing_students(self):
         url = reverse('lesson-detail', args=[self.lesson_1.id])
         data = {
-            'date': '01/05/2023 10:00:00',
+            'date': '01/01/2023 10:00:00',
             'teacher': 1,
             'study_subject': 1,
             'student_group': 1,
@@ -227,6 +246,24 @@ class TestLesson(TestCase):
                          )
 
         self.assertEqual([1], ser_data['missing_students'])
+
+    def test_update_incorrect_status(self):
+        url = reverse('lesson-detail', args=[self.lesson_1.id])
+        data = {
+            'date': '01/05/2023 10:00:00',
+            'teacher': 1,
+            'study_subject': 1,
+            'student_group': 1,
+            'status': True,
+        }
+
+        json_data = json.dumps(data)
+        response = self.client.put(url, data=json_data, content_type='application/json')
+
+        self.assertEqual(status.HTTP_400_BAD_REQUEST, response.status_code)
+        self.assertEqual(response.json()['status'],
+                         'Занятия нельзя завершить раньше назначенной даты.'
+                         )
 
     def test_delete(self):
         url = reverse('lesson-detail', args=[self.lesson_2.id])
